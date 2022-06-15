d6078d702959e1d29fdf029c344b35e39f7d5e2a
Esteban Alonso Barrios Araya <esteban.barrios@alumnos.inf.utfsm.cl>
2022-06-14 22:04:37 -0400

    intial proyect with rails 7

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'about', to: "pages#about"
+  get 'contact', to: "pages#contact"
+  get 'services', to: "pages#services"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#index"
+end
