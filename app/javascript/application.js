d8bb5561efb386d1535d62c5fa141bd30015dc99
Esteban Alonso Barrios Araya <esteban.barrios@sansano.usm.cl>
2022-06-15 19:34:38 -0400

    responsive navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+/* import "@hotwired/turbo-rails"
+import "controllers" */
 import 'bootstrap'
-import "custom/nav_controller"
+import "custom/hamburger"
---
+++ app/javascript/custom/hamburger.js
@@ -0,0 +1,5 @@
+
+document.querySelector('#nav-hamburguer').addEventListener('click', function () {
+  document.querySelector('.hamburguer-icon').classList.toggle('open');
+  document.querySelector('.nav-menu').classList.toggle('open');
+});  
