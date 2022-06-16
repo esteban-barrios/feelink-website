d73d1a1a53924f3d74244fc98786f99097a03875
Adrian Menares <adrianmenares22@gmail.com>
2022-06-16 13:50:14 -0400

    fix crear usuario

--- app/controllers/empresas_controller.rb
+++ app/controllers/empresas_controller.rb
@@ -15,9 +15,22 @@ class EmpresasController < ApplicationController
         @empresas = Empresa.all
     end
 
+    def agregar_usuario
+        @user = User.new()
+    end
+
+    def crear_usuario
+        @user = User.new(user_create_params)
+        @user.empresa_id = @empresa.id
+    end
+
     private
 
     def create_params
       params.require(:empresa).permit(:nombre, :direccion, :comuna, :oficina, :region)
     end
+
+    def user_create_params
+        params.require(:user).permit(:nombre, :email, :apellido, :telefono, :encargado)
+      end
 end
