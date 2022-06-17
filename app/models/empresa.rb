e45d07bb18c90be6704594d8353858240cb0c87b
Adrian Menares <adrianmenares22@gmail.com>
2022-06-17 17:59:30 -0400

    añadir validaciones de modelos

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -1,5 +1,13 @@
 class Empresa < ApplicationRecord
 
+
+    validates :nombre, presence: true
+    validates :direccion, presence: true
+    validates :comuna, presence: true
+    validates :region, presence: true
+    validates :rut, presence: false
+
+
     def empleados
         User.where(empresa_id: self.id).where(encargado: false).all
     end
